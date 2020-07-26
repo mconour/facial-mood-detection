9594a6d1e9ec6c45e6394bcddec9f6dbcf2b55a3
Michael Conour <michaelconour@gmail.com>
2020-07-26 16:44:12 -0700

    loaded all models used to detect face

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 const video = document.getElementById("video");
 
+// loaded all different models
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('./models')
+]).then(startVideo);
+
 
 // hook up webcam to video element
 startVideo = () => {
     navigator.getUserMedia({
-            video: {}
-        },
-        stream => video.srcObject = stream,
-        err => console.log(err)
+        video: {}
+    },
+    stream => video.srcObject = stream,
+    err => console.log(err)
     )
 }
 
