89d501bb6685313ca3b1051dc028da3d5460702b
Michael Conour <michaelconour@gmail.com>
2020-07-26 19:59:21 -0700

    fixed issue with canvas not aligning to face

--- main.js
+++ main.js
@@ -12,27 +12,39 @@ Promise.all([
 // hook up webcam to video element
 function startVideo() {
     navigator.getUserMedia({
-        video: {}
-    },
-    stream => video.srcObject = stream,
-    err => console.log(err)
+            video: {}
+        },
+        stream => video.srcObject = stream,
+        err => console.log(err)
     )
 }
 
 /* setting up the facial detection */
 video.addEventListener('play', () => {
-   
+
     // adding canvas element
-const canvas = faceapi.createCanvasFromMedia(video)
-document.body.append(canvas)
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
 
-// gets display size of current video
-const displaySize = {width: video.width, height: video.height}
+    // gets display size of current video
+    const displaySize = {
+        width: video.width,
+        height: video.height
+    }
 
+
+    faceapi.matchDimensions(canvas, displaySize)
 
     setInterval(async () => {
         // gets all faces inside of webcam image each time called
         const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
         console.log(detections)
+
+        // displays elements inside canvas
+        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+       faceapi.draw.drawDetections(canvas, resizedDetections)
+
     }, 100)
 })
